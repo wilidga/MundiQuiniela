fae481f28c04155611d5c9dde18289e5218caac0
wilidga <wilidga@hotmail.com>
2018-05-08 08:19:29 -0600

    Fix alert

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
-import { AlertController } from 'ionic-angular';
 
 
 @NgModule({
